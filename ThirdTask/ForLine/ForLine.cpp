d351ae5501ea110bd2e5a94ee6a6921c5ae58fce
Alena-pa <alena.pakhnusheva@gmail.com>
2025-11-28 18:21:19 +0300

    Add some changes

---
+++ ThirdTask/ForLine/ForLine.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+
+void countSort(vector<long long>& a, int n, long long digit) {
+    int size = a.size();
+    vector<long long> out(size);
+
+    if (n < 2) return;
+
+    vector<int> cnt(n, 0);
+
+    for (long long x : a) {
+        int d = (x / digit) % n;
+        cnt[d]++;
+    }
+
+    for (int i = 1; i < n; i++)
+        cnt[i] += cnt[i - 1];
+
+    for (int i = size - 1; i >= 0; i--) {
+        int d = (a[i] / digit) % n;
+        out[--cnt[d]] = a[i];
+    }
+
+    a = out;
+}
+
+void radixSort(vector<long long>& a, int n) {
+    long long base1 = 1;
+    long long base2 = 1LL * n;
+    long long base3 = 1LL * n * n;
+
+    countSort(a, n, base1);
+    countSort(a, n, base2);
+    countSort(a, n, base3);
+}
+
+vector<long long> makeRandom(int n) {
+    random_device rd;
+    mt19937_64 gen(rd());
+    long long maxv = 1LL * n * n * n - 1;
+
+    uniform_int_distribution<long long> dist(0, maxv);
+
+    vector<long long> a(n);
+    for (long long& x : a)
+        x = dist(gen);
+
+    return a;
+}
+
+void testSorting(int n) {
+    vector<long long> a = makeRandom(n);
+    vector<long long> b = a;
+
+    auto t1 = chrono::high_resolution_clock::now();
+    radixSort(a, n);
+    auto t2 = chrono::high_resolution_clock::now();
+
+    double t_radix = chrono::duration<double>(t2 - t1).count();
+
+    auto t3 = chrono::high_resolution_clock::now();
+    sort(b.begin(), b.end());
+    auto t4 = chrono::high_resolution_clock::now();
+
+    double t_std = chrono::duration<double>(t4 - t3).count();
+
+    if (a == b)
+        cout << "OK\n";
+    else
+        cout << "ERROR: sorting incorrect!\n";
+
+    cout << "\nResults of sorting\n";
+    cout << "Radix sort: " << t_radix << " sec\n";
+    cout << "std::sort:  " << t_std << " sec\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n = 50000;
+    testSorting(n);
+}
---
+++ ThirdTask/Strings/RadixSort.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+void countSort(vector<string>& arr, int pos) {
+    int n = arr.size();
+    vector<string> output(n);
+    int count[27] = { 0 };
+
+    for (int i = 0; i < n; i++) {
+        int index = (pos >= arr[i].size()) ? 0 : arr[i][pos] - 'a' + 1;
+        count[index]++;
+    }
+
+    for (int i = 1; i < 27; i++)
+        count[i] += count[i - 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+        int index = (pos >= arr[i].size()) ? 0 : arr[i][pos] - 'a' + 1;
+        output[count[index] - 1] = arr[i];
+        count[index]--;
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+void radixSort(vector<string>& arr) {
+    size_t maxLen = 0;
+    for (auto& s : arr)
+        if (s.size() > maxLen) maxLen = s.size();
+
+    for (int pos = maxLen - 1; pos >= 0; pos--)
+        countSort(arr, pos);
+}
+
+bool isSorted(const vector<string>& arr) {
+    for (size_t i = 1; i < arr.size(); ++i)
+        if (arr[i - 1] > arr[i])
+            return false;
+    return true;
+}
+
+string randomString(int len, mt19937& rng) {
+    uniform_int_distribution<int> dist('a', 'z');
+    string s(len, ' ');
+    for (int i = 0; i < len; ++i)
+        s[i] = char(dist(rng));
+    return s;
+}
+
+void testStringSorting(int n, int N_max) {
+    mt19937 rng(12345);
+    vector<string> data;
+
+    for (int i = 0; i < n; i++) {
+        uniform_int_distribution<int> lenDist(1, N_max);
+        int len = lenDist(rng);
+        data.push_back(randomString(len, rng));
+    }
+
+    vector<string> data_std = data;
+
+    // Radix Sort
+    auto start = chrono::high_resolution_clock::now();
+    radixSort(data);
+    auto end = chrono::high_resolution_clock::now();
+    cout << "Radix sort: " << chrono::duration<double>(end - start).count() << " sec\n";
+
+    if (!isSorted(data)) {
+        cout << "Error at Radix sort!\n";
+        return;
+    }
+
+    // std::sort
+    start = chrono::high_resolution_clock::now();
+    sort(data_std.begin(), data_std.end());
+    end = chrono::high_resolution_clock::now();
+    cout << "std::sort: " << chrono::duration<double>(end - start).count() << " sec\n";
+
+    if (!isSorted(data_std)) {
+        cout << "Error at std::sort!\n";
+        return;
+    }
+}
+
+int main() {
+    testStringSorting(100000, 10);
+    return 0;
+}
