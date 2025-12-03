92d37ee9896f9345a34fb5370307dad4c491c849
Alena-pa <alena.pakhnusheva@gmail.com>
2025-12-03 22:00:01 +0300

    fix some problems

--- firstTask/firstTask/ShellSort.cpp
+++ firstTask/firstTask/ShellSort.cpp
@@ -16,7 +16,7 @@ vector<long long> generateRandomArray(int size, long long maxValue) {
     return arr;
 }
 
-vector<long long> generateSmoothSteps(int n) {
+vector<long long> generateSmoothSteps(int sizeOfArray) {
     set<long long> numbers = { 1 };
     vector<long long> res;
 
@@ -24,7 +24,7 @@ vector<long long> generateSmoothSteps(int n) {
         long long cur = *numbers.begin();
         numbers.erase(numbers.begin());
 
-        if (cur >= n) break;
+        if (cur >= sizeOfArray) break;
         res.push_back(cur);
 
         numbers.insert(cur * 2);
@@ -36,62 +36,59 @@ vector<long long> generateSmoothSteps(int n) {
 void ShellSort(vector<long long>& array) {
     int size = array.size();
     vector<long long> steps = generateSmoothSteps(size);
+    reverse(steps.begin(), steps.end());
 
-    for (auto i = steps.rbegin(); i != steps.rend(); i++) {
-        long long gap = *i;
+    for (long long step : steps) {
+        for (int i = step; i < size; i++) {
+            int temp = array[i];
+            int j = i;
 
-        for (long long i = gap; i < size; i++) {
-            long long temp = array[i];
-            long long j = i;
-
-            while (j >= gap && array[j - gap] > temp) {
-                array[j] = array[j - gap];
-                j -= gap;
+            while (j >= step && array[j - step] > temp) {
+                array[j] = array[j - step];
+                j -= step;
             }
 
-            array[j] = gap;
+            array[j] = temp;
         }
     }
 }
 
-void test(vector<long long>& v, void (*sort_func)(vector<long long>&)) {
-    vector<long long> vec_std = v;
+void test(vector<long long>& array) {
+    vector<long long> arrayForStd = array;
 
-    auto start_std = chrono::steady_clock::now();
-    sort(vec_std.begin(), vec_std.end());
-    auto end_std = chrono::steady_clock::now();
-    chrono::duration<double> elapsed_std = end_std - start_std;
-    cout << "std::sort time: " << elapsed_std.count() << " s\n";
+    auto startStd = chrono::steady_clock::now();
+    sort(arrayForStd.begin(), arrayForStd.end());
+    auto endStd = chrono::steady_clock::now();
+    chrono::duration<double> elapsedStd = endStd - startStd;
+    cout << "std::sort time: " << elapsedStd.count() << " s\n";
 
-    auto start_custom = chrono::steady_clock::now();
-    sort_func(v);
-    auto end_custom = chrono::steady_clock::now();
-    chrono::duration<double> elapsed_custom = end_custom - start_custom;
-    cout << "Shell sort time: " << elapsed_custom.count() << " s\n";
+    auto startShell = chrono::steady_clock::now();
+    ShellSort(array);
+    auto endShell = chrono::steady_clock::now();
+    chrono::duration<double> elapsedShell = endShell - startShell;
+    cout << "Shell sort time: " << elapsedShell.count() << " s\n";
 
-    if (elapsed_std < elapsed_custom) {
-        double speedup = elapsed_custom.count() / elapsed_std.count();
+    if (elapsedStd < elapsedShell) {
+        double speedup = elapsedShell.count() / elapsedStd.count();
         cout << "std::sort is " << speedup << "x faster\n";
     }
     else {
-        double speedup = elapsed_std.count() / elapsed_custom.count();
+        double speedup = elapsedStd.count() / elapsedShell.count();
         cout << "Shell sort is " << speedup << "x faster\n";
     }
 }
 
-void test_generator(const vector<int>& sizes) {
+void testGenerator(const vector<int>& sizes) {
     for (int pow10 : sizes) {
-        long long n = pow(10, pow10);
-        cout << "\nTEST FOR N = 10^" << pow10 << " (" << n << " elements)\n";
+        long long sizeOfArray = pow(10, pow10);
+        cout << "\nTEST FOR N = 10^" << pow10 << " (" << sizeOfArray << " elements)\n";
 
-        auto vec = generateRandomArray(n, 1e8);
-        test(vec, shell_sort_custom);
-
-        cout << "--------------------------\n";
+        vector<long long> array = generateRandomArray(sizeOfArray, 1e8);
+        test(array);
     }
 }
 
 int main() {
     vector<int> test_sizes = { 3, 4, 5, 6 };
-    test_generator(test_sizes);
+    testGenerator(test_sizes);
 }
